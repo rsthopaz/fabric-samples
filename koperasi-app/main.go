eda9b4412778d7cc8f73c5efde29f0287c59196c
rsthopaz <thopazrosadi15@gmail.com>
2026-03-30 14:08:21 +0700

    add main.go

---
+++ koperasi-app/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"koperasi-app/blockchain"
+)
+
+func main() {
+	client, _:= blockchain.NewFabricClient()
+
+	// ADD ITEM
+	res, err := client.AddInventoryItem(
+		"100",
+		"BOX",
+		"Box Karton",
+		"Unit packaging",
+		"box",
+		1,
+		false,
+		"Quantity",
+		true,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Add result:", res)
+
+	// READ ITEM
+	read, err := client.ReadInventoryItem("100")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Read result:", read)
+}
